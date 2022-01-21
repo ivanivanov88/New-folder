f8d07453fa26b51a0386a6ec1dfa40a671b2dd2e
Ivan <ivanivanov88yho@yahoo.com>
2022-01-21 23:00:29 +0200

    Changes to index js and app js

--- nicefittrip/pages/_app.js
+++ nicefittrip/pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+import {Web3ReactProveder} from '@web3-react/core'
+import Web3 from 'web3'
+
+function getLibrary(provider){
+  return new Web3(provider)
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+  <Web3ReactProveder getLibrary = {getLibrary}>
+  <Component {...pageProps} />
+  </Web3ReactProveder>
+  
+  )
+  }
 
 export default MyApp
